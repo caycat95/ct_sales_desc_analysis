ec89950d99006d5def5546948e351ba848566073
Cayleigh Thalmann <cayleighthalmann@gmail.com>
2025-01-07 10:47:00 -0600

    Replaced initial setup with start of script.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+import pandas as pd
+
+# Will make this dynamic, hard-coding a practice dataset on my machine for right now.
+CSV_NAME = "~/supermarket_sales.csv"
+
+df = pd.read_csv(CSV_NAME)
+print(df)

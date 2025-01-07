eb2033b72e22bfa52ba9ededcf999572fcc22452
Cayleigh Thalmann <cayleighthalmann@gmail.com>
2025-01-07 11:15:26 -0600

    Divided script into functions including main

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ import pandas as pd
 # Will make this dynamic, hard-coding a practice dataset on my machine for right now.
 CSV_NAME = "~/supermarket_sales.csv"
 
-df = pd.read_csv(CSV_NAME)
-print(df)
+def pd_read_csv():
+    df = pd.read_csv(CSV_NAME)
+    return df
+
+def main():
+    df = pd_read_csv()
+    print(df)
+
+if __name__ == "__main__":
+    main()
+

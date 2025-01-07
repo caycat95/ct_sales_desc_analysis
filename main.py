e47508b55fda14802a947f76dd91f1978269d096
Cayleigh Thalmann <cayleighthalmann@gmail.com>
2025-01-07 12:10:59 -0600

    Started implementing data analysis functions.

--- main.py
+++ main.py
@@ -7,9 +7,31 @@ def pd_read_csv():
     df = pd.read_csv(CSV_NAME)
     return df
 
+def calc_total_profit(df):
+    return df['gross income'].sum()
+
+def calc_total_rev(df):
+    return df['Total'].sum()
+
+def get_unique_branches(df):
+    return df['Branch'].unique()
+
+def divide_branch(df, branch):
+    return df.loc[df['Branch'] == branch]
+
+# A majority of what is in main right now is for testing/brainstorming.
 def main():
     df = pd_read_csv()
-    print(df)
+    total_profit = calc_total_profit(df)
+    total_rev = calc_total_rev(df)
+    unique_branches = get_unique_branches(df)
+    print(unique_branches)
+    branch = divide_branch(df, unique_branches[0])
+    branch_trev = calc_total_rev(branch)
+    print(branch_trev)
+    print(total_rev)
+
+    
 
 if __name__ == "__main__":
     main()
